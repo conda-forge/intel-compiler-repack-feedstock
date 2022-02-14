231a4f8d83ff1521ecf7b44c2ac10d436722d828
Isuru Fernando <isuruf@gmail.com>
2022-02-14 15:23:30 -0600

    Update opencl-rt-bld.bat

--- recipe/opencl-rt-bld.bat
+++ recipe/opencl-rt-bld.bat
@@ -11,8 +11,6 @@ echo %LIBRARY_LIB%\intel-ocl-cpu\intelocl64.dll> %LIBRARY_PREFIX%\etc\OpenCL\ven
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
 :: This will allow them to be run on environment activation.
 FOR %%F IN (activate deactivate) DO (
-    if not exist %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
-    if errorlevel 1 exit 1
     copy %RECIPE_DIR%\opencl-win-%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
 	:: We also copy .sh scripts to be able to use them
     :: with POSIX CLI on Windows.
