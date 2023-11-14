c232bb2588005aa4efd4ea1c27c29ec43e1ef73d
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-11-14 08:04:23 -0600

    Update sources to 2024.0 compiler bits
    Modified opencl-rt-bld.bat to reflect that OCL CPU RT DLL files
    have moved from LIBRARY_LIB to LIBRARY_BIN
    
    Made path changes in opencl-rt-bld.bat
    
    Made path changes in test command for intel-opencl-rt on
    Windows.

--- recipe/opencl-rt-bld.bat
+++ recipe/opencl-rt-bld.bat
@@ -1,11 +1,14 @@
 @echo on
-mkdir "%LIBRARY_PREFIX%\intel-ocl-cpu"
-set "src=%SRC_DIR%\%PKG_NAME%\Library\lib"
-robocopy /E "%src%" "%LIBRARY_LIB%\intel-ocl-cpu"
+if not exist "%LIBRARY_BIN%\intel-ocl-cpu" mkdir "%LIBRARY_BIN%\intel-ocl-cpu"
+set "src1=%SRC_DIR%\%PKG_NAME%\Library\bin"
+robocopy /E "%src1%" "%LIBRARY_BIN%\intel-ocl-cpu"
 if %ERRORLEVEL% GEQ 8 exit 1
 
-mkdir "%LIBRARY_PREFIX%\etc\OpenCL\vendors\"
-echo %LIBRARY_LIB%\intel-ocl-cpu\intelocl64.dll> %LIBRARY_PREFIX%\etc\OpenCL\vendors\intel-ocl-cpu.icd
+set "src2=%SRC_DIR%\%PKG_NAME%\Library\lib"
+robocopy /E "%src2%" "%LIBRARY_LIB%"
+if %ERRORLEVEL% GEQ 8 exit 1
 
 :: populate CL_CONFIG_TBB_DLL_PATH = entry in cl.cfg setting it to %LIBRARY_BIN%
-python %RECIPE_DIR%\set_tbb_dll_path.py %LIBRARY_LIB%\intel-ocl-cpu\cl.cfg
+python %RECIPE_DIR%\set_tbb_dll_path.py %LIBRARY_BIN%\intel-ocl-cpu\cl.cfg
+mkdir "%LIBRARY_PREFIX%\etc\OpenCL\vendors\"
+echo %LIBRARY_BIN%\intel-ocl-cpu\intelocl64.dll> %LIBRARY_PREFIX%\etc\OpenCL\vendors\intel-ocl-cpu.icd
