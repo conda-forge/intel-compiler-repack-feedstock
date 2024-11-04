fb46a40a015a1f062baa3c4448ef1437f23c1e29
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2024-11-04 17:38:03 +0000

    Add compiler_shared to windows

--- recipe/repack.bat
+++ recipe/repack.bat
@@ -3,5 +3,13 @@ set "src=%SRC_DIR%\%PKG_NAME%"
 robocopy /E "%src%" "%PREFIX%"
 if %ERRORLEVEL% GEQ 8 exit 1
 
+:: TODO: do we need compiler_shared as a separate package? Is there any other use
+:: except dpcpp_impl_linux-64 ? May be there are some conda forge pacakges we can
+:: link to.
+if "%PKG_NAME%" == "dpcpp_impl_win-64" (
+  rmdir /s %SRC_DIR%/compiler_shared/info
+  xcopy /E %SRC_DIR%/compiler_shared/. %src%
+)
+
 :: replace old info folder with our new regenerated one
 rd /s /q %PREFIX%\info
