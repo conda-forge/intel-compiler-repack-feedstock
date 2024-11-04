95641d81ad8f338b0cd371dd2d5dc5d517d137ad
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2024-11-04 14:27:21 -0600

    Do not include OpenCL loader libraries on Windows
    This is parallel to how it is done on Linux. This allows libraries
    from khronos-opencl-loader to be used and avoids clobbering errors

--- recipe/opencl-rt-bld.bat
+++ recipe/opencl-rt-bld.bat
@@ -4,9 +4,15 @@ set "src1=%SRC_DIR%\%PKG_NAME%\Library\bin"
 robocopy /E "%src1%" "%LIBRARY_BIN%\intel-ocl-cpu"
 if %ERRORLEVEL% GEQ 8 exit 1
 
+:: Make way for using conda-forge's OpenCL loader
+del "%LIBRARY_BIN%\intel-ocl-cpu\OpenCL.dll"
+
 set "src2=%SRC_DIR%\%PKG_NAME%\Library\lib"
 robocopy /E "%src2%" "%LIBRARY_LIB%"
 if %ERRORLEVEL% GEQ 8 exit 1
+
+:: Make way for using conda-forge's OpenCL loader
+del "%LIBRARY_LIB%\OpenCL.lib"
 
 :: populate CL_CONFIG_TBB_DLL_PATH = entry in cl.cfg setting it to %LIBRARY_BIN%
 python %RECIPE_DIR%\set_tbb_dll_path.py %LIBRARY_BIN%\intel-ocl-cpu\cl.cfg
