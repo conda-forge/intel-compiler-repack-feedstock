94993c47884c6c1db3ba3ec16f43827a46763253
Evseniia <70146207+ekomarova@users.noreply.github.com>
2025-04-07 18:59:11 +0200

    Introduce fortran compiler for windows (#57)
    * Introduce fortran compiler for windows

--- recipe/install_activate.sh
+++ recipe/install_activate.sh
@@ -6,5 +6,12 @@ else
   echo "unknown platform"
   exit 1
 fi
-sed "s/@HOST@/$HOST/g" "${RECIPE_DIR}/scripts/activate-dpcpp.sh" > "${PREFIX}/etc/conda/activate.d/~activate-dpcpp.sh"
-cat "${PREFIX}/etc/conda/activate.d/~activate-dpcpp.sh"
+if [[ "$PKG_NAME" == "dpcpp_linux-64" ]]; then
+  sed "s/@HOST@/$HOST/g" "${RECIPE_DIR}/scripts/activate-dpcpp.sh" > "${PREFIX}/etc/conda/activate.d/~activate-dpcpp.sh"
+  cat "${PREFIX}/etc/conda/activate.d/~activate-dpcpp.sh"
+fi
+
+if [[ "$PKG_NAME" == "ifx_linux-64" ]]; then
+  sed "s/@HOST@/$HOST/g" "${RECIPE_DIR}/scripts/activate-ifx.sh" > "${PREFIX}/etc/conda/activate.d/~activate-ifx.sh"
+  cat "${PREFIX}/etc/conda/activate.d/~activate-ifx.sh"
+fi
